d8fbf339322e2c7ab26de044d3cac5b1e540071f
Lazaro Alonso <lazarus.alon@gmail.com>
2023-08-05 20:25:04 +0200

    fixes deploy and copy code

--- docs/make.jl
+++ docs/make.jl
@@ -27,8 +27,8 @@ makedocs(
     build=joinpath(@__DIR__, "docs")
 )
 
-deploydocs(; repo="https://github.com/TidierOrg/TidierStrings.jl", push_preview=true,
+deploydocs(; repo="github.com/TidierOrg/TidierStrings.jl.git", push_preview=true,
     deps=Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-material",
         "pymdown-extensions", "mkdocstrings", "mknotebooks",
-        "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2", "mkdocs-video"),
+        "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2"),
     make=() -> run(`mkdocs build`), target="site", devbranch="main")
