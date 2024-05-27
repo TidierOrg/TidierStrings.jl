ae69771cfbe59e678a1a34483cad7d76dbe32a06
webofceco <mapleselijah9@gmail.com>
2024-05-26 23:48:37 -0400

    locate function docstrings

--- src/strings_docstrings.jl
+++ src/strings_docstrings.jl
@@ -600,9 +600,48 @@ Int64[]
 
 const docstring_str_locate = 
 """
+    str_locate(string::AbstractString, pattern::Union{AbstractString, Regex})
+
+Returns the index of the first occurrence of a pattern in a string.
+
+Arguments
+- `string`: Input string.
+- `pattern`: The pattern to search for. Can be a string or a regular expression.
+
+A tuple `(start, end)` where `start` is the position at the start of the match and `end` is the position of the end.
+
+Examples
+```jldoctest
+julia> fruit = ["apple", "banana", "pear", "pineapple"]; str_locate(fruit[1], "e")
+(5, 5)
+
+julia> fruit = ["apple", "banana", "pear", "pineapple"]; str_locate(fruit[2], "a")
+(1, 1)
+```
 """
 const docstring_str_locate_all = 
 """
+    str_locate_all(string::AbstractString, pattern::Union{AbstractString, Regex})
+
+Returns the indices of all occurrences of a pattern in a string.
+
+Arguments
+- `string`: Input string.
+- `pattern`: The pattern to search for. Can be a string or a regular expression.
+
+A vector of tuples `(start, end)` where `start` is the position at the start of the match and `end` is the position of the end.
+
+Examples
+```jldoctest
+julia> fruit = ["apple", "banana", "pear", "pineapple"]; str_locate_all(fruit[1], "e")
+1-element Vector{Tuple{Int64, Int64}}:
+ (5, 5)
+
+julia> fruit = ["apple", "banana", "pear", "pineapple"]; str_locate_all(fruit[2], "a")
+2-element Vector{Tuple{Int64, Int64}}:
+ (1, 1)
+ (5, 5)
+```
 """
 
 const docstring_str_replace_missing = 
