d27b5f3e13ad0f58279aff3dfa28bb68a317bc01
Randall Boyes <33524191+rdboyes@users.noreply.github.com>
2024-12-04 09:25:15 -0500

    Update docstrings.jl

--- src/docstrings.jl
+++ src/docstrings.jl
@@ -1,12 +1,14 @@
 const docstring_str_detect  =
 """
-    str_detect(string::String, pattern::Union{String, Regex})
+    str_detect(string::String, pattern::Union{String, Regex}; overlap::Bool=false)
 
 Determine if a string contains a certain pattern.
 
 # Arguments
 - `string`: The string to check.
-pattern: A string or a regular expression to find within the string.
+- `pattern`: A string or a regular expression to find within the string.
+- `overlap`: Whether the pattern can overlap (default false)
+
 The pattern can include special logic:
 
 Use | to represent "or" (e.g., "red|blue" matches any string that contains "red" or "blue").
